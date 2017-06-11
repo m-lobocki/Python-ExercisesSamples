3b11aeed33e44feefe504696b9cb0b46513b87b2
mati9344 <mlobocki@outlook.com>
2017-06-11 14:12:42 +0200

    2 exercises 3 lvl

---
+++ ex22.py
@@ -0,0 +1,24 @@
+# Write a program to compute the frequency of the words from the input. The output should output after sorting the key
+# alphanumerically.
+# Suppose the following input is supplied to the program:
+# New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3.
+# Then, the output should be:
+# 2:2
+# 3.:1
+# 3?:1
+# New:1
+# Python:5
+# Read:1
+# and:1
+# between:1
+# choosing:1
+# or:2
+# to:1
+
+words = sorted(input().split())
+words_frequency = {}
+for word in words:
+    words_frequency[word] = words.count(word)
+
+for key, value in words_frequency.items():
+    print(f'{key}:{value}')
