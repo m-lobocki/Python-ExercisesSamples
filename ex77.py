4be433c196c7ad61b1b812407908346f9c8cef0c
mati9344 <mlobocki@outlook.com>
2017-06-29 22:29:25 +0200

    5 exercises

---
+++ ex77.py
@@ -0,0 +1,5 @@
+# Please write a program to output a random number, which is divisible by 5 and 7, between 0 and 10 inclusive using
+# random module and list comprehension.
+
+import random
+print(random.choice([x for x in range(11) if x % 5 == 0 and x % 7 == 0]))
